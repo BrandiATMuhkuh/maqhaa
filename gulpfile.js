bad33d95927af4e7f34978aae6527b538146b13d
BrandiATMuhkuh <juergen@tagaprice.org>
2015-06-21 20:46:06 +1200

    added a gulp deploy system

--- gulpfile.js
+++ gulpfile.js
@@ -20,6 +20,7 @@ var merge = require('merge-stream');
 var path = require('path');
 var fs = require('fs');
 var glob = require('glob');
+var ghPages = require('gulp-gh-pages');
 
 var AUTOPREFIXER_BROWSERS = [
   'ie >= 10',
@@ -236,6 +237,11 @@ gulp.task('default', ['clean'], function (cb) {
     ['jshint', 'images', 'fonts', 'html'],
     'vulcanize', 'precache',
     cb);
+});
+
+gulp.task('deploy', function() {
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages());
 });
 
 // Load tasks for web-component-tester
